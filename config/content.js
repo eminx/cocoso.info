a85917fcd4c3167019aaea6f7b531ca7d4d40d24
Emin Durak <emin@tuta.io>
2020-07-07 23:01:52 +0530

    some updates to content

--- config/content.js
+++ config/content.js
@@ -1,7 +1,7 @@
 const intro = [
-  `Cocoso is a set of tools to create your public website; as well as an intranet to keep track of all your practicalities in one place.`,
-  `It's a web based, open-source software for collaboratively facilitating internal/external operations within a community. It empowers web presence and accessibility regarding autonomous actions, artistic/cultural activities and sharing of common resources. It's made for autonomous and creative groups, suitable for any type of collaboration such as: festivals, artist-run initiatives, non-for-profits, families, small or medium businesses, alternative schools, permaculture sites, yoga spaces or your grandmother’s farm that will turn into an open air museum.`,
-  `Cocoso is a community-driven, resource-aware and open source project. Strong artistic aspirations, social motives and economic inclusion needs have paved the path at its earlier stages. Currently, it learns from multiple communities where it's used to help build and maintain their unique projects. These communities are made up of artists, artist-run initiatives, alternative currency projects - and hopefully yours.`,
+  `Cocoso is a set of tools to create a public website, as well as an intranet to keep track of all your practicalities in one place.`,
+  `It's web based, open-source software for collaboratively facilitating internal/external operations within a community. It empowers web presence and accessibility regarding autonomous actions, artistic/cultural activities and sharing of common resources. It's made for autonomous and creative groups, suitable for any type of collaboration such as: conferences, festivals, artist-run initiatives, non-for-profits, families, small or medium businesses, alternative schools, permaculture sites, yoga spaces or your grandmother’s farm that will turn into an open air museum.`,
+  `Cocoso is a community-driven, resource-aware and open source project. Strong artistic aspirations, social motives and economic inclusion needs have paved the path at its earlier stages. Currently, it builds up by learning from multiple communities where it's used to help build and maintain their unique projects. These communities are made up of artists, artist-run initiatives, alternative currency projects and cocreative places.`,
   `Cocoso doesn't feature tools for spying on users for tracking their data. Each member of any community owns their data, and are free to share only as much as they wish. No information is shared with any third party service or organisation, the data is owned by the community. You run projects of your community and their data on your own terms.`
 ];
 
@@ -44,8 +44,8 @@ const resources = [
 const resourceTags = ['Resource Sharing', 'Management of Commons'];
 
 const calendar = [
-  `Cocoso’s web calendar can be displayed to show Monthly/Weekly/Daily events and resource updates. Users can navigate between these timeframes with a single tap. Each calendar activity can correspond to a resource-utilisation activity or a community event. Multiplicity of resources and events are welcome at the calendar. Calendar is built for flexibility, transparency and simplicity to fit your unique needs and collective scenarios. `,
-  `Each entry in calendar could be assigned a different category such as a resource booked or a type of activity such as yoga. It'd be useful for both presenting different kinds of public activity entries as well as different kinds of bookings. Like every each feature in Cocoso, it's intentionally made very abstract so that it can be applied to different use cases.`
+  `Cocoso’s web calendar can be displayed to show Monthly/Weekly/Daily events and resource updates. Users can navigate between these timeframes with a single tap. Each calendar activity can correspond to a resource-utilisation activity or a community event. Multiplicity of resources and events are welcome at the calendar. Calendar is built for flexibility, transparency and simplicity to fit unique needs and collective scenarios. `,
+  `Each entry in calendar could be assigned a different category such as a resource booked or a type of activity such as yoga. It's useful for both presenting different kinds of public activity entries as well as different internal/private bookings. Like every each feature in Cocoso, it's intentionally made very abstract so that it can be applied to different use cases.`
 ];
 
 const calendarTags = [
@@ -57,10 +57,9 @@ const calendarTags = [
 ];
 
 const processes = [
-  `Process is an abstract term to cover multiple activities conducted by a group -- either virtual or in the real world. It allows members of a process to log all their relevant data from creating till proceeding a meaningful process together to share a space, document, or conduct a meeting with another member.`,
-  `Process enables management of processes such as training, social creation or learning activities -- such as recurring yoga sessions, or school training sessions. Just add the details of your process and your simple forum, documents list and dates are ready in one page.`,
-  `This feature basically includes most of the digital tools for running, for example a learning, training, cocreation or facilitation process. Like a school, or a continuous/recurrent yoga session, or a cooperative activity of any kind.`,
-  `You can basically keep your main information (image and text), members list, a simple discussion forum, documents, and meeting dates all in one page.`
+  `Process is an abstract term to represent a concept, and a set of conceptually driven activities pursuing that concept conducted by a group - either virtually or actually. Under a process, individuals can embark collaborative activities and have all the practical details such as basic info and dates documented in one page.`,
+  `Process enables management of processes such as training, social creation or learning activities -- such as recurring yoga sessions, or school training sessions, or any sort of cocreation and continuously evolving process. Admins can start it up by adding details of the concept with an image and some text. Then a simple forum, documents list, members and dates are ready in one page.`,
+  `This feature basically includes most of the digital tools for running, for example a learning, training, cocreation or facilitation process. Like a school, or a continuous/recurrent yoga session, or a cooperative activity of any kind.`
 ];
 
 const processTags = [
@@ -74,8 +73,7 @@ const processTags = [
 
 const works = [
   `A Work is a visual (or audiovisual) creation of an artist, researcher, designer, craftsperson or reseller to be showcased to the community. Details of the work, with images, sound files and videos can be shared within a category of creator’s choice. A work is a unique entry, associated with its creator -- a perfect tool for portfolios, and marketplaces.`,
-  `One can also create a work and save it privately for internal use and documentation. The primary purpose it is built for is to enable artists and craftspeople to publish and communicate what they do with the rest of the world, as well as document for their own reference`,
-  `Images to be uploaded are automatically resized on the client browser before they are uploaded to AWS S3 buckets.`
+  `One can also create a work and save it privately for internal use and documentation. The primary purpose it is built for is to enable artists and craftspeople to publish and communicate what they do with the rest of the world, as well as document for their own reference.`
 ];
 
 const workTags = ['Portfolio', 'Marketplace', 'Documentation', 'Presentation'];
@@ -84,8 +82,7 @@ const members = [
   `If you're admin of an organisation, you can find and filter through members of the organisation. You can verify or unverify them for being able to do certain activities such as creating activities in the program/calendar or creating a process.`,
   `For example if you run a yoga studio, you'll have both your teachers and students in one platform. Then you will verify your teachers for them to be able to create their activities at the home page, whereas students will only be able participate (RSVP) to existing activities/processes.`,
   `So, necessarily, privileges can be meaningfully distributed amongst your community.`,
-  `Every user can easily create an account and continue their operations from there.`,
-  `Passwords are automatically encrypted in the database so system admins won't be able to track them.`
+  `Every user can easily create an account and continue their operations from there. Passwords are automatically encrypted in the database so system admins won't be able to track them.`
 ];
 
 const memberTags = [
@@ -97,7 +94,7 @@ const memberTags = [
 ];
 
 const info = [
-  `This is a very simple page, basically. The simplest CMS you could ever think of...`,
+  `This feature is basically being able to create a set of simple, static pages. The simplest CMS you could ever think of...`,
   `With this feature one can easily create a page only with a title and a description (with rich text editing). Title becomes the slug with a little modification like About Us becoming /page/about-us and that's pretty much it. Each page is automatically added to navigation as just another static page.`
 ];
 
